05392f685ad97e60daa2d6125f51352a1727285d
actusnileh <actusnileh@gmail.com>
2024-09-19 13:07:21 +0300

    лаб работа 2

--- backend/src/services/iter_matrix_service.py
+++ backend/src/services/iter_matrix_service.py
@@ -2,44 +2,57 @@ class IterMatrixService:
 
     @staticmethod
     def matrix_chain_order(p) -> tuple[list[list[int]], list[list[int]]]:
-        n = len(p) - 1  # Количество матриц
+        n = len(p) - 1  # Количество матриц (На единицу меньше, чем кол-во элементов)
         matrix = [[0] * n for _ in range(n)]  # Таблица для минимальных стоимостей
         k = [[0] * n for _ in range(n)]  # Таблица для хранения индексов разбиений
 
-        # length - длина цепочки матриц
-        for length in range(2, n + 1):
-            for i in range(n - length + 1):
-                j = i + length - 1
+        for length in range(
+            2,
+            n + 1,
+        ):  # Перебираем длины от 2 до n (длину 1 не учитываем, так как это одна матрица)
+            for i in range(
+                n - length + 1,
+            ):  # Для каждой подцепочки длины `length` перебираем её начало (индекс i)
+                j = i + length - 1  # конец подцепочки
                 matrix[i][j] = float("inf")
                 for s in range(i, j):
-                    cost = matrix[i][s] + matrix[s + 1][j] + p[i] * p[s + 1] * p[j + 1]
-                    if cost < matrix[i][j]:
+                    cost = (
+                        matrix[i][s] + matrix[s + 1][j] + p[i] * p[s + 1] * p[j + 1]
+                    )  # Вычисляем стоимость разбиения цепочки по формуле (от i до s и от s+1 до j)
+                    if (
+                        cost < matrix[i][j]
+                    ):  # Если текущая стоимость меньше, чем уже сохранённая, обновляем её
                         matrix[i][j] = cost
-                        k[i][j] = s  # Сохраняем индекс разбиения
+                        k[i][j] = s  # Сохраняем индекс
 
         return matrix, k
 
     @staticmethod
     def optimal_parens(s, i, j) -> str:
-        # Используем стек для обработки пар индексов и знака открытия/закрытия
-        stack = [(i, j, False)]  # Последний элемент в кортеже - открывать ли скобку
+        # Стек для обработки пар индексов (i, j) и информации о скобке
+        stack = [(i, j, False)]
         result = []
 
         while stack:
-            i, j, to_open = stack.pop()
+            i, j, to_open = (
+                stack.pop()
+            )  # Извлекаем пару индексов и флаг открывающей/закрывающей скобки
 
-            if i == j:
+            if i == j:  # Если это одна матрица, добавляем её
                 result.append(
                     f"A{i + 1}",
-                )  # Добавляем матрицу A(i+1), т.к. индексация с 0
+                )
             else:
                 if not to_open:
-                    # Добавляем текущий блок скобок и раскладываем его на подзадачи
+                    # Добавляем открывающую скобку и подзадачи для подцепочек
                     result.append("(")
-                    stack.append((i, j, True))  # Для закрытия после рекурсивных вызовов
+                    # Добавляем задачу для закрытия скобки
+                    stack.append((i, j, True))
+                    # Добавляем задачи для правой и левой частей цепочки
                     stack.append((s[i][j] + 1, j, False))  # Правая часть цепочки
                     stack.append((i, s[i][j], False))  # Левая часть цепочки
                 else:
+                    # Если дошли до конца цепочки, добавляем закрывающую скобку
                     result.append(")")
 
         return "".join(result)
